1e39c42efe16eec5395479924585538eac8d1a4b
harjeevansingh <harjeevansingh01@gmail.com>
2018-03-20 19:44:59 +0530

    added spychat

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome To The Spychat")
+spy_name = input("Choose Your SpyName")
+salutation = input("What should be your salutation Mr or Ms ")
+print("Alright " + salutation + "." + spy_name + " I'd like to know a little bit more about you...")
+
