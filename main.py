145b6113db50acc31395aab381df5215b7831ed5
harjeevansingh <harjeevansingh01@gmail.com>
2018-03-21 20:55:57 +0530

    Added comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 print("Welcome To The Spychat")
 spy_name = input("Choose Your SpyName")
+
+# now we will ask for the salutation
 salutation = input("What should be your salutation Mr or Ms ")
 print("Alright " + salutation + "." + spy_name + " I'd like to know a little bit more about you...")
 
