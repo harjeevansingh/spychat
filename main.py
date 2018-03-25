0c4eeceb12d199877198f9b6de8948f3836e3528
harjeevansingh <harjeevansingh01@gmail.com>
2018-03-25 18:50:39 +0530

    added the default details file for import

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
-#### Lecture 1 ###
+# Let the spying begin xD
+from spy_details import spy_age, spy_name, salutation, spy_rating
 
 print("Welcome To The Spychat ")
 
 # Knowing whether the spy wants default name
-def_name = input("Would you like a new Spy Name? y or n")
+def_name = input("Would you like to continue with default  Spy profile? y or n")
 
-if def_name == "y":
+if def_name == "n":
     spy_name = input("Choose Your SpyName")
+    if len(spy_name) > 0:
+        # now we will ask for the salutation
+        salutation = input("What should be your salutation Mr or Ms ")
+        print("Alright %s.%s I'd like to know a little bit more about you..." % (salutation, spy_name))
+        # asking other details
+        spy_age = int(input("What is your age?"))
+
+        # checking the spy eligibility
+        if spy_age in range(13, 51):
+            spy_rating = float(input("Enter your rating."))
+        else:
+            print("Sorry you are not suitable to be a spy.")
+            exit()
+
+    else:
+        print("You have to enter your name to proceed.")
+        exit()
+
 
 else:
-    # setting default spy name
-    spy_name = "Spy Singh"
+    pass
 
-
-if len(spy_name) > 0:
-    # now we will ask for the salutation
-    salutation = input("What should be your salutation Mr or Ms ")
-    print("Alright " + salutation + "." + spy_name + " I'd like to know a little bit more about you...")
-
-else:
-    print("You have to enter your name to proceed.")
-
-# asking other details
-spy_age = int(input("What is your age?"))
-
-#default rating
-spy_rating = 0.0
-
-# checking the spy eligibility
-if spy_age in range(13, 51):
-    spy_rating = float(input("Enter your rating."))
-else:
-    print("Sorry you are not suitable to be a spy.")
 
 # Giving messages acco. to the ratings
 if spy_rating > 4.5:
-    print("You are Pro!!!")
+    print("You are a Pro!!!")
 elif spy_rating > 3.5 and spy_rating <= 4.5:
-    print("you are Awesome!!!")
-elif spy_rating >=2.5 and spy_rating <= 3.5:
+    print("you are perfect!!!")
+elif spy_rating >= 2.5 and spy_rating <= 3.5:
     print("You have it in you.")
 else:
     print("You need to improve.")
 
-print("So, "+salutation+"."+spy_name+", you are "+str(spy_age)+" old with a "+str(spy_rating)+" rating.")
+print("So, %s.%s, you are %d old with a %.1f rating." % (salutation, spy_name, spy_age, spy_rating))
+
+
+
+
 
 
 
---
+++ spy_details.py
@@ -0,0 +1,5 @@
+spy_name = "Spy Singh"
+salutation = "Mr"
+spy_age = 34
+spy_rating = 3.7
+some_variable = "just for practice"
