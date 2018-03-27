9d2c527dd0a9d3c9281e83656e8b61fea0fb35dd
harjeevansingh <harjeevansingh01@gmail.com>
2018-03-27 17:14:35 +0530

    Added the add friend menu

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # Let the spying begin xD
 from spy_details import spy_age, spy_name, salutation, spy_rating
 
-status_updates = ["Hey, there!!", "Available", "Sleeping"]
-
-
 print("Welcome To The Spychat ")
 
 # Knowing whether the spy wants default name
@@ -15,6 +12,7 @@ if def_name.upper() == "N":
         # now we will ask for the salutation
         salutation = input("What should be your salutation Mr or Ms ")
         print("Alright %s.%s I'd like to know a little bit more about you..." % (salutation, spy_name))
+
         # asking other details
         spy_age = int(input("What is your age?"))
 
@@ -24,12 +22,9 @@ if def_name.upper() == "N":
         else:
             print("Sorry you are not suitable to be a spy.")
             exit()
-
     else:
         print("You have to enter your name to proceed.")
         exit()
-
-
 else:
     pass
 
@@ -47,7 +42,6 @@ else:
     exit()
 
 
-
 def app_menu(spy_name, spy_age, spy_rating ):
     show_menu = True
     current_status_message = None
@@ -56,11 +50,17 @@ def app_menu(spy_name, spy_age, spy_rating ):
     menu_choices = "Select the option. \n 1. Add a Status Update \n 2. Add a Friend \n 3. Send a Secret Message \n 4. Read a Secret Message \n 5. Read chats from a 'User' \n 6. Close application"
     while show_menu:
         choice = input(menu_choices)
-
         if choice == "1":
             current_status_message = add_status(current_status_message)
+        elif choice == "2":
+            print("You have ", add_friend(), "friends.")
         else:
-            show_menu = False
+            exit()
+
+
+# Setting the status updates
+status_updates = ["Hey, there!!", "Available", "Sleeping"]
+
 
 def add_status(current_status_message):
     if current_status_message == None:
@@ -86,7 +86,29 @@ def add_status(current_status_message):
             status_updates.append(current_status_message)
             return current_status_message
 
+# Adding friends to the profile
+friends_name = []
+friends_age = []
+friends_rating = []
+friends_are_online = []
 
+
+def add_friend():
+    new_name = input("Enter the friend's name.")
+    new_salutation = input("Salutation for friend's name.")
+    new_age = int(input("What's the friend's age?"))
+    new_rating = float(input("Enter the friend's rating."))
+    new_name = new_name+"."+new_salutation
+
+    # checking the friend's eligibility
+    if len(new_name) > 0 and new_age in range(13,51) and new_rating > spy_rating:
+        friends_name.append(new_name)
+        friends_age.append(new_age)
+        friends_rating.append(new_rating)
+        friends_are_online.append(True)
+    else:
+        print("Sorry, your friend is not eligible to be a spy.")
+    return len(friends_name)
 
 
 
